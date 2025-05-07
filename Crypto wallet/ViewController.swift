bfc560d0171fe088f1a3290e646414104f3c3f31
FedorBebinov <fsbebinov@edu.hse.ru>
2025-05-07 23:30:18 +0300

    Initial Commit

---
+++ Crypto wallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Crypto wallet
+//
+//  Created by Fedor Bebinov on 07.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
